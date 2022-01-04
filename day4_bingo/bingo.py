e88cbc08079a4e2638448a6b238fc59374957b72
Yohann Jacob Sandvik <yjs@datarespons.no>
2022-01-04 10:26:13 +0100

    changes

--- day4_bingo/bingo.py
+++ day4_bingo/bingo.py
@@ -15,7 +15,9 @@ bingo_boards = []
 while (bingo_boards_start_line < file_end_line):
     bingo_lists = []
     for i in range(5):
-        bingo_lists.append(file_content[bingo_boards_start_line + i][:-2].split(' '))
+        single_bingo_row = file_content[bingo_boards_start_line + i]
+        bingo_lists.append(single_bingo_row[:-2].split(' '))
     bingo_boards_start_line += 6
+    bingo_boards.append(bingo_lists)
 
-print(bingo_lists[0])
+print(bingo_boards[0])
